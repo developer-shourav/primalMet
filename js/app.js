4c4e8c2db84cd048c6452e9944acd02ec0fddefd
root <root@localhost>
2023-02-14 23:17:38 +0600

    auto publish 1676395057492

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Add react app code here
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
